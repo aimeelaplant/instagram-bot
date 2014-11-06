5b91d5ca0e321033d23c573b65f43137c31d2d07
Aimee LaPlant <aimee@aimeelaplant.com>
2014-11-06 13:28:46 -0600

    Updated comments

--- bot.php
+++ bot.php
@@ -1,22 +1,21 @@
 <?
 
 /*
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+Example for use of aimee.instagram.class.php
 */
 
 include 'aimee.instagram.class.php';
 
 $tags = array('englishbulldog', 'bulldog', 'igbulldogs_worldwide');
 
-
 foreach ($tags as $tag) {
+    // Set the access token
     $ig = new Instagram('access_token', true);
+    // Get the ID of the media object from a tag.
     $media_id = $ig->getIDByTag($tag);
+    // Likes media object.
     $ig->likeMedia($media_id);
-
     unset($ig);
-    sleep(2);
 }
 
 
