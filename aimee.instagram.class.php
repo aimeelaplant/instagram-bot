32f7e70712b64bd34a433791eb45abce94c4c113
Aimee LaPlant <aimee@aimeelaplant.com>
2015-01-05 17:02:36 -0600

    better :)
    made code more modular and we will now do the shuffling in the example.php file.

--- aimee.instagram.class.php
+++ aimee.instagram.class.php
@@ -1,131 +1,103 @@
 <?php
 
 class Instagram {
+    
+    private $_access_token;
+    private $_endpoints = array(
+            'tag_search' => 'https://api.instagram.com/v1/tags/%s/media/recent?access_token=%s',
+            'media_like' => 'https://api.instagram.com/v1/media/%s/likes',
+            'user_feed'  => 'https://api.instagram.com/v1/users/self/feed?access_token=%s',
+            'login' => 'https://api.instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=code&scope=likes',
+            'oauth_token' => 'https://api.instagram.com/oauth/access_token',
 
-    public function __construct($access_token, $testing) {
-        $this->access_token = $access_token;
-        $this->testing = $testing;
-        $this->endpoint_urls = array(
-            'search_tag'    => 'https://api.instagram.com/v1/tags/%s/media/recent?access_token=%s',
-            'like_media'    => 'https://api.instagram.com/v1/media/%s/likes',
-            'comment_media' => 'https://api.instagram.com/v1/media/%s/comments',
-        );
+    );
+
+    public function __construct($client_id=null, $client_secret=null, $redirect_uri=null, $log=false) {
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+        $this->redirect_uri = $redirect_uri;
     }
 
-    public function log($log) {
-        $file = "log.txt";
-        $content = file_get_contents("log.txt");
-        $content .= $log . "\n";
-        file_put_contents($file, $content);
+    // login url for user to grant permission to client.
+    public function getLoginURL() {
+        return sprintf($this->_endpoints['login'], $this->client_id, $this->redirect_uri);
     }
 
-    public function curl($url, $data_type = null, $post_params = null) {
+    public function setAccessToken($_access_token) {
+        $this->_access_token = $_access_token;
+    }
 
+    public function getAccessToken() {
+        return $this->_access_token;
+    }
+
+    // function to request url and return json data.
+    protected function curl($url, $post_params = null) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-
-        if ($data_type == "post") {
+        // parameters for POST data
+        if ($post_params) {
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_params));
         }
-
         $results = curl_exec($ch);
         curl_close($ch);
-        return $results;
+        return json_decode($results, true);
     }
-
-
-//1 comment per run. 
-    public function getIDByTag($tag) {
-        // Returns ONE random ID
-        // For now, we'll set num_results to 30 (max return results for Instagram API);
-        $num_results = 30;
-        $url = sprintf($this->endpoint_urls['search_tag'], $tag, $this->access_token);
-        $data = $this->curl($url);
-        $data = json_decode($data, true);
-
-/*
-        if ($data['meta']['code'] == 200) {
-            $media_ids = array();
-            for ($i=0; $i < $num_results; $i++) {
-                $media_ids[] = $data['data'][$i]['id'];
-            }
-            // Shuffle results
-            shuffle($media_ids);
-            // Return ONE media id.
-            return $media_ids[0];
-*/
-        if ($data['meta']['code'] == 200) {
-
-            shuffle($data['data']);
-            return $data['data'][0]['id'];
-
-        } else {
-            $this->log("API Error: ".print_r($data,true));
-        }
-
-        // log data on error code
-        if ($data['meta']['code'] == 429) {
-            $this->log("API Error: ".print_r($data,true));
-
-        } else {
-            $log = print_r($data);
-            $this->logData($log);
-            exit;
-        }
-
-    }
-
-    public function likeMedia($media_id) {
-        // Likes a media object given the media ID.
-        $url = sprintf($this->endpoint_urls['like_media'], $media_id);
-        $params = array('access_token' => $this->access_token);
-        $data = $this->curl($url, $data_type='post', $post_params=$params);
-        $data = json_decode($data, true);
-        
-        if ($data['meta']['code'] == 200) {
-            $this->log("$this->access_token liked post $media_id");
-            #$log = "Success! Liked post ID " . $media_id . ". ";
-            #echo $log;
-        } 
-        if ($data['meta']['code'] == 429) {
-            $this->log("API Error: ".print_r($data,true));
-            #print_r($data);
-        #    $log = print_r($data);
-        #    $this->logData($lost);
-        #    exit;
-
-        }
-
-    }
-
-
-    public function commentMedia($media_id, $comment_text) {
-        // Comments on media object.
-        $url = sprintf($this->endpoint_urls['comment_media'], $media_id);
+    
+    
+    public function getOAuthToken($code) {
         $params = array(
-            'access_token' => $this->access_token, 
-            'text'         => $comment_text
+            'client_id' => $this->client_id, 
+        'client_secret' => $this->client_secret,
+        'grant_type' => 'authorization_code',
+        'redirect_uri' => $this->redirect_uri,
+        'code' => $code
         );
+        $result = $this->curl($this->_endpoints['oauth_token'], $post_params=$params);
+        return $result['access_token'];
+  }
 
-        $data = $this->curl($url, $data_type='post', $post_params=$params);
+    public function log($log) {
+        $date = new DateTime();
+        $file = "log.txt";
+        $content = file_get_contents($file);
+        // timestamp and log message
+        $content .= $date->format('U = Y-m-d H:i:s') . ": " . $log . "\n";
+        file_put_contents($file, $content);
+    }
+    
+    // Searches a tag from Instagram's public tag feed.
+    public function searchTag($tag) {
+        $url = sprintf($this->_endpoints['tag_search'], $tag, $this->getAccessToken());
+        $data = $this->curl($url);
+        return $data;
+    }
 
-        if ($data['meta']['code'] == 200) {
-            $this->log("$this->access_token commented ".$comment_text." ID ".$media_id);
-        }
-        if ($data['meta']['code'] == 429) {
-            $this->log("API Error: ".print_r($data,true));
-        }
+    // Gets the feed of the user (your own account)
+    public function getUserFeed() {
+        $url = sprintf($this->_endpoints['user_feed'], $this->getAccessToken());
+        return $this->curl($url);
+    }
 
-        if ($this->testing) {
-            echo $log;
+    // Likes a media object given a media ID (e.g. a photo or video).
+    public function likeMedia($id)  {
+        $url = sprintf($this->_endpoints['media_like'], $id);
+        $params = array('access_token' => $this->getAccessToken());
+        $results = $this->curl($url, $post_params=$params);
+
+        // log response
+        if ($log) {
+            if ($results['meta']['code'] == 200)   {
+                $this->log($this->getAccessToken() . 'liked object ID' . $id);
+            } else {
+                $this->log($results['meta']);
+            }
         }
     }
 
 }
-
-?>
